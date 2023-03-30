de83be05049325b74efdbdde12cb53f27ddf9a9f
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2023-03-30 14:35:15 +0500

    added another route

--- src/main/java/com/demo/org/Main.java
+++ src/main/java/com/demo/org/Main.java
@@ -5,13 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class Main {
     public static void main(String[] args) {
         // This runs the main application
         SpringApplication.run(Main.class, args);
-    //  System.out.println("Hello Dum Dums");
     }
 
     // decorators starting with "*Mapping" indicates that these methods will be exposed to the port
@@ -21,10 +22,18 @@ public class Main {
     }
 
     // This method returns a JSON response. We did this, just by using MyRestResponse as the return type for this method
-    @GetMapping("/rest")
+    @GetMapping("/sample_rest")
     public MyRestResponse sample() {
         return new MyRestResponse("ceci est une réponse en REST");
     }
     // Then we used this line to introduce the return type mentioned in the above method
     record MyRestResponse(String sample){}
+
+    // This method is the same as above but this time it returns an integer
+    @GetMapping("/base_api")
+    public BaseApiResponse baseApiResponse() {
+        return new BaseApiResponse(42);
+    }
+    record BaseApiResponse(Integer baseApiResponse){}
+
 }
